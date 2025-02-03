34e24cead8321bdc85cd61103f91b50f0456a7a5
jiyoungnim <cloundyon31@gmail.com>
2025-02-03 19:34:49 +0900

    📝24. Preparing the App For State Management

--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -1,16 +1,20 @@
+import NewPost from "./NewPost";
 import Post from "./Post";
 import classes from "./PostList.module.css";
 
 function PostList() {
   return (
-    <ul className={classes.posts}>
-      <Post author="jiyoung" body="REACT IS FUN!" />
-      <Post author="suzy" body="JS IS FUN!" />
-      <Post author="suzy" body="JS IS FUN!" />
-      <Post author="suzy" body="JS IS FUN!" />
-      <Post author="suzy" body="JS IS FUN!" />
-      <Post author="suzy" body="JS IS FUN!" />
-    </ul>
+    <>
+      <NewPost />
+      <ul className={classes.posts}>
+        <Post author="jiyoung" body="REACT IS FUN!" />
+        <Post author="suzy" body="JS IS FUN!" />
+        <Post author="suzy" body="JS IS FUN!" />
+        <Post author="suzy" body="JS IS FUN!" />
+        <Post author="suzy" body="JS IS FUN!" />
+        <Post author="suzy" body="JS IS FUN!" />
+      </ul>
+    </>
   );
 }
 
